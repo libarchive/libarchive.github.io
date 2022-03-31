9ddc1df79a9d0ed0fb5cb1421984522f396ccc0f
removedporn <86824510+removedporn@users.noreply.github.com>
2022-04-01 05:39:55 +0800

    Delete google-code directory

--- google-code/issue-125/comment-17/checkExists.c
+++
@@ -1,7 +0,0 @@
-#include <sys/sysmacros.h>
-
-int main()
-{
-  return major(256);
-}
-
--- google-code/issue-300/comment-0/bug.py
+++
@@ -1,4 +0,0 @@
-import zipfile
-zf = zipfile.ZipFile('out.zip', 'w')
-zf.writestr('file.txt', 'This is a test.\n')
-zf.close()
